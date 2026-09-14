06d8a64bc3ee89f603c28fc777ca697272566702
Migz2cool12 <migz2cool12@gmail.com>
2026-09-14 23:03:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cashfinder-v1";
+const CACHE_NAME = "cashfinder-v2";
 
 const FILES = [
   "./",
@@ -8,18 +8,83 @@ const FILES = [
   "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES);
-    })
-  );
-});
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener(
+  "install",
+  event => {
+
+    event.waitUntil(
+
+      caches.open(
+        CACHE_NAME
+      ).then(
+        cache =>
+          cache.addAll(
+            FILES
+          )
+      )
+
+    );
+
+    self.skipWaiting();
+
+  }
+);
+
+
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+
+      caches.keys().then(
+        cacheNames =>
+
+          Promise.all(
+
+            cacheNames
+              .filter(
+                name =>
+                  name !== CACHE_NAME
+              )
+              .map(
+                name =>
+                  caches.delete(name)
+              )
+
+          )
+
+      )
+
+    );
+
+    self.clients.claim();
+
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    event.respondWith(
+
+      caches.match(
+        event.request
+      ).then(
+        response => {
+
+          return (
+            response ||
+            fetch(event.request)
+          );
+
+        }
+      )
+
+    );
+
+  }
+);
